cc46c6950a482ffddae1e7357f23effc1e4432ea
Eric Friedman <ericsean01@gmail.com>
2021-03-02 21:45:23 -0500

    first commit

---
+++ app/src/main/java/com/example/cis3515assignment4/SelectionActivity.java
@@ -0,0 +1,17 @@
+package com.example.cis3515assignment4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Set label
+        //getActionBar().setTitle("@string/selection_activity");
+    }
+}
